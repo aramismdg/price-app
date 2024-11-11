74afac46f5d1595c99aeacb124deda3a4b5e662b
Mariano Godoy <50596171+marianogodoy82@users.noreply.github.com>
2024-11-10 22:57:23 -0300

    initial project

---
+++ price-app/src/test/java/com/challenge/priceapp/PriceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.priceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAppApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
